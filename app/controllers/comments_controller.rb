64ba911577475c4b776135d487caf8f271dcb458
John Bagley <jdbagley@gmail.com>
2014-07-26 23:48:53 -0400

    Add comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,19 +4,19 @@ class CommentsController < ApplicationController
   end
 
   def new
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.new
+    @user = User.find(params[:user_id])
+    @comment = Comment.new
   end
 
   def create
-    article = Article.find(params[:article_id])
-    comment = article.comments.create(comment_params)
-    redirect_to article_path(article)
+    user = User.find(params[:user_id])
+    comment = user.comments.create(comment_params)
+    redirect_to article_path(:article_id)
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content).merge(user_id: current_user.id)
+    params.require(:comment).permit(:content)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :article
-  has_many :likes, as: :likable
 end
